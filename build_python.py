b1186eb73eec2a27c654110985ffc74240aa4b2d
Siong Chin <schin@ccdc.cam.ac.uk>
2024-01-05 10:15:16 +0000

    Quotes in PYTHON_CONFIGURE_OPTS (PYAPI-3505)

--- build_python.py
+++ build_python.py
@@ -198,7 +198,7 @@ def install_pyenv_version(version):
             #python_build_env['CPPFLAGS'] = f'{subprocess.check_output(["pkg-config", "--cflags", "openssl11"]).decode().strip()} -I{python_version_destdir()}/include'
             python_build_env['LDFLAGS'] = f'-L{python_version_destdir()}/lib -lsqlite3 {subprocess.check_output(["pkg-config", "--libs", "openssl11"]).decode().strip()}'
             python_build_env['CPPFLAGS'] = f'-I{python_version_destdir()}/include {subprocess.check_output(["pkg-config", "--cflags", "openssl11"]).decode().strip()}'
-            python_build_env['PYTHON_CONFIGURE_OPTS']=f"--enable-shared LD_RUN_PATH={python_version_destdir()}/lib LDFLAGS=-L{python_version_destdir()}/lib CPPFLAGS=-I{python_version_destdir()}/include"
+            python_build_env['PYTHON_CONFIGURE_OPTS']=f"--enable-shared LD_RUN_PATH={python_version_destdir()}/lib LDFLAGS=\"{python_build_env['LDFLAGS']}\" CPPFLAGS=\"{python_build_env['CPPFLAGS']}\""
             subprocess.run('sudo -E env', shell=True, check=True, env=python_build_env)
             subprocess.run(f'sudo -E /tmp/pyenvinst/plugins/python-build/bin/python-build -v {version} {python_version_destdir()}', shell=True, check=True, env=python_build_env)
             return
