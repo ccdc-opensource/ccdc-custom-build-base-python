403aec3e7e1f88d85d924440b2d28ee865a75c3c
Siong Chin <schin@ccdc.cam.ac.uk>
2024-06-21 20:06:34 +0100

    Why exit code 1? (BLD-6874)

--- build_python.py
+++ build_python.py
@@ -151,20 +151,10 @@ def install_prerequisites():
             subprocess.run('sudo dnf config-manager --enable powertools', shell=True, check=True)
             subprocess.run('sudo dnf install -y epel-release', shell=True, check=True)
             subprocess.run(
-                    'sudo dnf install -y git zlib-devel bzip2-devel tk-devel tcl-devel libffi-devel libsqlite3x-devel openssl3-devel',
+                    'sudo dnf install -y git zlib-devel bzip2-devel tk-devel tcl-devel libffi-devel libsqlite3x-devel openssl3-devel readline-devel xz-devel',
                     shell=True,
                     check=True
                     )
-        #        if centos():
-        #            subprocess.run('sudo yum update -y', shell=True, check=True)
-        #            subprocess.run('sudo yum install -y https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm', shell=True, check=True)
-        #            subprocess.run('sudo yum install -y epel-release', shell=True, check=True)
-        #            subprocess.run('sudo yum install -y findutils gcc zlib-devel bzip2 bzip2-devel readline-devel openssl11-libs openssl11-devel tkinter tk tk-devel tcl-devel xz xz-devel libffi-devel patch powershell', shell=True, check=True)
-        #            # See https://jira.ccdc.cam.ac.uk/browse/BLD-5684
-        #            subprocess.run(f'sudo mkdir -p {python_version_destdir()}', shell=True)
-        #            subprocess.run(f'sudo chown $(id -u) {python_version_destdir()}; echo "chown $(id -u) {python_version_destdir()}"', shell=True)
-        #            SqlitePackage().build()
-
         if ubuntu():
             subprocess.run('sudo apt-get -y update', shell=True, check=True)
             subprocess.run('sudo apt-get -y dist-upgrade', shell=True, check=True)
@@ -199,20 +189,13 @@ def install_pyenv_version(version):
     if linux():
         python_build_env['PATH']=f"/tmp/pyenvinst/plugins/python-build/bin:{python_build_env['PATH']}"
         #python_build_env['PYENV_DEBUG'] = '1'
-#        if centos():
-#            python_build_env['PATH']=f"{python_version_destdir()}/bin:{python_build_env['PATH']}"
-#            python_build_env['PYTHON_CONFIGURE_OPTS']="--enable-shared"
-#            subprocess.run(f"sed -i 's#\"${{!PACKAGE_CONFIGURE_OPTS_ARRAY}}\" $CONFIGURE_OPTS ${{!PACKAGE_CONFIGURE_OPTS}} || return 1#\"${{!PACKAGE_CONFIGURE_OPTS_ARRAY}}\" $CONFIGURE_OPTS --enable-shared LD_RUN_PATH={python_version_destdir()}/lib LD_LIBRARY_PATH={python_version_destdir()}/lib LDFLAGS=\"-L{python_version_destdir()}/lib -L/usr/lib64/openssl11 -lssl -lcrypto -L/usr/lib64 -ltcl8.5 -ltk8.5 -lz -lm -ldl -lpthread\" CPPFLAGS=\"-I{python_version_destdir()}/include -I/usr/include/openssl11\" ${{!PACKAGE_CONFIGURE_OPTS}} || return 1#' /tmp/pyenvinst/plugins/python-build/bin/python-build", shell=True, check=True, env=python_build_env)
-#            subprocess.run(f'grep CONFIGURE_OPTS /tmp/pyenvinst/plugins/python-build/bin/python-build', shell=True, check=True, env=python_build_env)
-#            subprocess.run(f'sudo -E /tmp/pyenvinst/plugins/python-build/bin/python-build -v {version} {python_version_destdir()}', shell=True, check=True, env=python_build_env)
-#            return
-    try:
-        subprocess.run(f'sudo env "PATH=$PATH" python-build -v {version} {python_version_destdir()}', shell=True, check=True, env=python_build_env)
-    except subprocess.CalledProcessError as e:
-        for filepath in Path('/tmp').glob('python-build.*.log'):
-            print(filepath)
-            print(filepath.read_text())
-        raise e
+#    try:
+    subprocess.run(f'sudo env "PATH=$PATH" python-build -v {version} {python_version_destdir()}', shell=True, check=True, env=python_build_env)
+#    except subprocess.CalledProcessError as e:
+#        for filepath in Path('/tmp').glob('python-build.*.log'):
+#            print(filepath)
+#            print(filepath.read_text())
+#        raise e
 
 
 def output_archive_filename():
