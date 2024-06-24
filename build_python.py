04920d7ff73c60b97550bac91ca7b839abeed931
Siong Chin <schin@ccdc.cam.ac.uk>
2024-06-24 08:43:35 +0100

    Use pathlib (BLD-6874)

--- build_python.py
+++ build_python.py
@@ -155,9 +155,9 @@ def install_prerequisites():
                     shell=True,
                     check=True
                     )
-            #subprocess.run('ls -l /usr/include/openssl', shell=True, check=True)
-            subprocess.check_output('ls -l /usr/lib64', shell=True, check=True)
-            subprocess.check_output('ls -l /usr/bin', shell=True, check=True)
+            print(Path('/usr/include').glob('openssl*'))
+            print(Path('/usr/lib64').glob('lib*'))
+            print(Path('/usr/bin').glob('openssl*'))
         if ubuntu():
             subprocess.run('sudo apt-get -y update', shell=True, check=True)
             subprocess.run('sudo apt-get -y dist-upgrade', shell=True, check=True)
